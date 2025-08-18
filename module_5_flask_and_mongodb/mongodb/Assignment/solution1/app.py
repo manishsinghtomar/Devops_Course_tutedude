979940f1b24c60265a2a8991ac3b702f916345be
Manish Singh Tomar <manishtomar00mt@gmail.com>
2025-08-18 17:29:53 +0530

    app.py added

--- module_5_flask_and_mongodb/mongodb/Assignment/solution1/app.py
+++ module_5_flask_and_mongodb/mongodb/Assignment/solution1/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify
+import json
 import os
-import requests
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
+@app.route('/api')
 def get_data():
-   response = requests.get('')
-    return json_response
-
+    DATA_FILE = os.path.join(os.path.dirname(__file__), 'data.json')
+    with open(DATA_FILE, 'r') as file:
+        data = json.load(file)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
